4f7b2ff68a99b47fa14cf0fec30416fe128206e3
Aynur <aynr.fsjf@outlook.com>
2022-10-06 12:35:57 +0300

    Added registration operation to the system. (JWT)

--- src/kodlama.io.Devs/Application/Services/Repositories/IOperationClaimRepository.cs
+++ src/kodlama.io.Devs/Application/Services/Repositories/IOperationClaimRepository.cs
@@ -1,5 +1,5 @@
 ﻿using Core.Persistence.Repositories;
-using Domain.Entities;
+using Core.Security.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace Application.Services.Repositories
 {
-    public interface ITechnologyRepository : IAsyncRepository<Technology>, IRepository<Technology>
+    public interface IOperationClaimRepository : IAsyncRepository<OperationClaim>, IRepository<OperationClaim>
     {
     }
 }
